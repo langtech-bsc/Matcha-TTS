1f0da668cbdd72325e1aa1e1671031b8f2662c60
AlexPeiroLilja <alex.peiro.lilja@gmail.com>
2026-02-19 15:31:19 +0100

    Update symbols.py
    update symbol dictionary for graphemes

--- matcha/text/symbols.py
+++ matcha/text/symbols.py
@@ -3,15 +3,20 @@
 Defines the set of symbols used in text input to the model.
 """
 _pad = "_"
-_punctuation = ';:,.!?¡¿—…"«»“”() '  # added ()
+# _punctuation = ';:,.!?¡¿—…"«»“”() '  # added '()' Matcha v2
+_punctuation = ';:,.!?¡¿—…"«»“”()- '  # added '()' and '-' Matcha graphemes
+#_punctuation = ';:,.!?¡¿—…"«»“” ' # Matcha v1
 _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 _letters_ipa = (
     "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
 )
-
+_letters_accented = "àáèéìíòóùú·üïöñ’#´"
 
 # Export all symbols:
 symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+symbols_graphemes = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa) + list(_letters_accented)
+# symbols_graphemes = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
 
+print("symbols vocabulary size: ", len(symbols_graphemes))
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
